99b7461dea8b2a278eae024949c8a49b27179bab
JustCarmen <carmen@justcarmen.nl>
2015-01-29 07:17:40 +0100

    Rename 'Administration' to 'Control panel'

--- module.php
+++ module.php
@@ -378,7 +378,7 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 
 		<!-- ADMIN PAGE CONTENT -->
 		<ol class="breadcrumb small">
-			<li><a href="admin.php"><?php echo WT_I18N::translate('Administration'); ?></a></li>
+			<li><a href="admin.php"><?php echo WT_I18N::translate('Control panel'); ?></a></li>
 			<li><a href="admin_modules.php"><?php echo WT_I18N::translate('Module administration'); ?></a></li>
 			<li class="active"><?php echo $this->getTitle(); ?></li>
 		</ol>
