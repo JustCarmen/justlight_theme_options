867e3a1cc4e089c7ac82405a23bf4754e6c95e7a
JustCarmen <carmen@justcarmen.nl>
2015-02-05 12:25:04 +0100

    Namespaces and GPL3 - following webtrees base.
    Refactor classes to use namespaces, as per PSR-4.  Replace GPL2 with GPL3
    https://github.com/fisharebest/webtrees/commit/a25f0a04682c4c39c1947220c90af4118c713952

--- module.php
+++ module.php
@@ -1,61 +1,56 @@
 <?php
-/*
- * JustLight Theme Options Module
- *
- * webtrees: Web based Family History software
- * Copyright (C) 2014 webtrees development team.
- * Copyright (C) 2014 JustCarmen.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
+namespace Webtrees;
+
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2015 webtrees development team
+ * Copyright (C) 2015 JustCarmen
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use WT\Auth;
-use WT\Log;
+use Zend_Translate;
 
-class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_Config {
+class justlight_theme_options_WT_Module extends Module implements ModuleConfigInterface {
 
 	public function __construct() {
 		parent::__construct();
 		// Load any local user translations
 		if (is_dir(WT_MODULES_DIR . $this->getName() . '/language')) {
 			if (file_exists(WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.mo')) {
-				WT_I18N::addTranslation(
+				I18N::addTranslation(
 					new Zend_Translate('gettext', WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.mo', WT_LOCALE)
 				);
 			}
 			if (file_exists(WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.php')) {
-				WT_I18N::addTranslation(
+				I18N::addTranslation(
 					new Zend_Translate('array', WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.php', WT_LOCALE)
 				);
 			}
 			if (file_exists(WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.csv')) {
-				WT_I18N::addTranslation(
+				I18N::addTranslation(
 					new Zend_Translate('csv', WT_MODULES_DIR . $this->getName() . '/language/' . WT_LOCALE . '.csv', WT_LOCALE)
 				);
 			}
 		}
 	}
 
-	// Extend WT_Module
+	// Extend Module
 	public function getTitle() {
-		return /* I18N: Name of a module  */ WT_I18N::translate('JustLight Theme Options');
+		return /* I18N: Name of a module  */ I18N::translate('JustLight Theme Options');
 	}
 
-	// Extend WT_Module
+	// Extend Module
 	public function getDescription() {
-		return /* I18N: Description of the module */ WT_I18N::translate('Set options for the JustLight theme within the admin interface');
+		return /* I18N: Description of the module */ I18N::translate('Set options for the JustLight theme within the admin interface');
 	}
 
 	// Set default module options
@@ -88,49 +83,49 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 	private function getDefaultMenu() {
 		$menulist = array(
 			'compact'	 => array(
-				'title'		 => WT_I18N::translate('View'),
+				'title'		 => I18N::translate('View'),
 				'label'		 => 'compact',
 				'sort'		 => '0',
 				'function'	 => 'menuCompact'
 			),
 			'media'		 => array(
-				'title'		 => WT_I18N::translate('Media'),
+				'title'		 => I18N::translate('Media'),
 				'label'		 => 'media',
 				'sort'		 => '0',
 				'function'	 => 'menuMedia'
 			),
 			'homepage'	 => array(
-				'title'		 => WT_I18N::translate('Home page'),
+				'title'		 => I18N::translate('Home page'),
 				'label'		 => 'homepage',
 				'sort'		 => '1',
 				'function'	 => 'menuHomePage'
 			),
 			'charts'	 => array(
-				'title'		 => WT_I18N::translate('Charts'),
+				'title'		 => I18N::translate('Charts'),
 				'label'		 => 'charts',
 				'sort'		 => '3',
 				'function'	 => 'menuChart'
 			),
 			'lists'		 => array(
-				'title'		 => WT_I18N::translate('Lists'),
+				'title'		 => I18N::translate('Lists'),
 				'label'		 => 'lists',
 				'sort'		 => '4',
 				'function'	 => 'menuLists'
 			),
 			'calendar'	 => array(
-				'title'		 => WT_I18N::translate('Calendar'),
+				'title'		 => I18N::translate('Calendar'),
 				'label'		 => 'calendar',
 				'sort'		 => '5',
 				'function'	 => 'menuCalendar'
 			),
 			'reports'	 => array(
-				'title'		 => WT_I18N::translate('Reports'),
+				'title'		 => I18N::translate('Reports'),
 				'label'		 => 'reports',
 				'sort'		 => '6',
 				'function'	 => 'menuReports'
 			),
 			'search'	 => array(
-				'title'		 => WT_I18N::translate('Search'),
+				'title'		 => I18N::translate('Search'),
 				'label'		 => 'search',
 				'sort'		 => '7',
 				'function'	 => 'menuSearch'
@@ -140,7 +135,7 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 	}
 
 	public function menuJustLight($menulist) {
-		$modules = WT_Module::getActiveMenus();
+		$modules = Module::getActiveMenus();
 		// add newly activated modules to the menu
 		$sort = count($menulist) + 1;
 		foreach ($modules as $module) {
@@ -179,13 +174,13 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 		$folders = WT_Query_Media::folderList();
 		foreach ($folders as $key => $value) {
 			if ($key == null && empty($value)) {
-				$folderlist[$MEDIA_DIRECTORY] = strtoupper(WT_I18N::translate(substr($MEDIA_DIRECTORY, 0, -1)));
+				$folderlist[$MEDIA_DIRECTORY] = strtoupper(I18N::translate(substr($MEDIA_DIRECTORY, 0, -1)));
 			} else {
 				if (count(glob(WT_DATA_DIR . $MEDIA_DIRECTORY . $value . '*')) > 0) {
 					$folder = array_filter(explode("/", $value));
 					// only list first level folders
 					if (!empty($folder) && !array_search($folder[0], $folderlist)) {
-						$folderlist[$folder[0] . '/'] = WT_I18N::translate($folder[0]);
+						$folderlist[$folder[0] . '/'] = I18N::translate($folder[0]);
 					}
 				}
 			}
@@ -228,7 +223,7 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 		return $status;
 	}
 
-	// Extend WT_Module_Config
+	// Extend ModuleConfigInterface
 	public function modAction($mod_action) {
 		switch ($mod_action) {
 			case 'admin_config':
@@ -245,15 +240,15 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 
 	// Reset all settings to default
 	private function jl_reset() {
-		WT_DB::prepare("DELETE FROM `##module_setting` WHERE setting_name LIKE 'JL%'")->execute();
+		Database::prepare("DELETE FROM `##module_setting` WHERE setting_name LIKE 'JL%'")->execute();
 		Log::addConfigurationLog($this->getTitle() . ' reset to default values');
 	}
 
 	// Radio buttons
 	private function radio_buttons($name, $selected) {
 		$values = array(
-			0	 => WT_I18N::translate('no'),
-			1	 => WT_I18N::translate('yes'),
+			0	 => I18N::translate('no'),
+			1	 => I18N::translate('yes'),
 		);
 
 		return radio_buttons($name, $values, $selected, 'class="radio-inline"');
@@ -261,19 +256,18 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 
 	private function config() {
 
-		if (WT_Filter::postBool('save') && WT_Filter::checkCsrf()) {
-			$NEW_JL_OPTIONS = WT_Filter::postArray('NEW_JL_OPTIONS');
-			$NEW_JL_OPTIONS['MENU'] = $this->sortArray(WT_Filter::postArray('NEW_JL_MENU'), 'sort');
+		if (Filter::postBool('save') && Filter::checkCsrf()) {
+			$NEW_JL_OPTIONS = Filter::postArray('NEW_JL_OPTIONS');
+			$NEW_JL_OPTIONS['MENU'] = $this->sortArray(Filter::postArray('NEW_JL_MENU'), 'sort');
 
 			$this->setSetting('JL_OPTIONS', serialize($NEW_JL_OPTIONS));
 			Log::addConfigurationLog($this->getTitle() . ' config updated');
 		}
 
-		require WT_ROOT . 'includes/functions/functions_edit.php';
-		$controller = new WT_Controller_Page;
+		$controller = new PageController;
 		$controller
 			->restrictAccess(Auth::isAdmin())
-			->setPageTitle(WT_I18N::translate('Options for the JustLight theme'))
+			->setPageTitle(I18N::translate('Options for the JustLight theme'))
 			->pageHeader();
 
 		$controller->addInlineJavaScript('
@@ -366,19 +360,19 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 
 		<!-- ADMIN PAGE CONTENT -->
 		<ol class="breadcrumb small">
-			<li><a href="admin.php"><?php echo WT_I18N::translate('Control panel'); ?></a></li>
-			<li><a href="admin_modules.php"><?php echo WT_I18N::translate('Module administration'); ?></a></li>
+			<li><a href="admin.php"><?php echo I18N::translate('Control panel'); ?></a></li>
+			<li><a href="admin_modules.php"><?php echo I18N::translate('Module administration'); ?></a></li>
 			<li class="active"><?php echo $this->getTitle(); ?></li>
 		</ol>
 		<h2><?php echo $this->getTitle(); ?></h2>
 		<form action="<?php echo $this->getConfigLink(); ?>" enctype="multipart/form-data" name="configform" method="post" class="form-horizontal">
 			<input type="hidden" value="1" name="save">
-			<?php echo WT_Filter::getCsrf(); ?>
+			<?php echo Filter::getCsrf(); ?>
 			<div id="accordion" class="panel-group">
 				<div id="panel1" class="panel panel-default">
 					<div class="panel-heading">
 						<h4 class="panel-title">
-							<a href="#collapseOne" data-target="#collapseOne" data-toggle="collapse"><?php echo WT_I18N::translate('Options'); ?></a>
+							<a href="#collapseOne" data-target="#collapseOne" data-toggle="collapse"><?php echo I18N::translate('Options'); ?></a>
 						</h4>
 					</div>
 					<div class="panel-collapse collapse in" id="collapseOne">
@@ -386,17 +380,17 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 							<!-- COMPACT MENU -->
 							<div id="compact-menu" class="form-group form-group-sm">
 								<label class="control-label col-sm-4">
-									<?php echo WT_I18N::translate('Use a compact menu?'); ?>
+									<?php echo I18N::translate('Use a compact menu?'); ?>
 								</label>
 								<div class="col-sm-8">
 									<?php echo $this->radio_buttons('NEW_JL_OPTIONS[COMPACT_MENU]', $this->options('compact_menu')); ?>
-									<p class="small text-muted"><?php echo WT_I18N::translate('In the compact “View”-menu the menus for Charts, Lists, Calendar and (optionally) Reports will be merged together.'); ?></p>
+									<p class="small text-muted"><?php echo I18N::translate('In the compact “View”-menu the menus for Charts, Lists, Calendar and (optionally) Reports will be merged together.'); ?></p>
 								</div>
 							</div>
 							<!-- REPORTS -->
 							<div id="reports" class="form-group form-group-sm">
 								<label class="control-label col-sm-4">
-									<?php echo WT_I18N::translate('Include the reports topmenu in the compact \'View\' topmenu?'); ?>
+									<?php echo I18N::translate('Include the reports topmenu in the compact \'View\' topmenu?'); ?>
 								</label>
 								<div class="col-sm-8">
 									<?php echo $this->radio_buttons('NEW_JL_OPTIONS[COMPACT_MENU_REPORTS]', $this->options('compact_menu_reports')); ?>
@@ -405,21 +399,21 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 							<!-- MEDIA MENU -->
 							<div id="media-menu" class="form-group form-group-sm">
 								<label class="control-label col-sm-4">
-									<?php echo WT_I18N::translate('Media menu in topmenu'); ?>
+									<?php echo I18N::translate('Media menu in topmenu'); ?>
 								</label>
 								<div class="col-sm-8">
 									<?php echo $this->radio_buttons('NEW_JL_OPTIONS[MEDIA_MENU]', $this->options('media_menu')); ?>
-									<p class="small text-muted"><?php echo WT_I18N::translate('If this option is set the media menu will be moved to the topmenu. The names of first level media folders from your media folder on the server will be used as submenu items of the new media menu. Warning: these submenu items are not translated automatically. Use a custom language file to translate your menu items. Read the webrees WIKI for more information.'); ?></p>
+									<p class="small text-muted"><?php echo I18N::translate('If this option is set the media menu will be moved to the topmenu. The names of first level media folders from your media folder on the server will be used as submenu items of the new media menu. Warning: these submenu items are not translated automatically. Use a custom language file to translate your menu items. Read the webrees WIKI for more information.'); ?></p>
 								</div>
 							</div>
 							<!-- SUBFOLDERS -->
 							<div id="subfolders" class="form-group form-group-sm">
 								<label class="control-label col-sm-4">
-									<?php echo WT_I18N::translate('Include subfolders'); ?>
+									<?php echo I18N::translate('Include subfolders'); ?>
 								</label>
 								<div class="col-sm-8">
 									<?php echo $this->radio_buttons('NEW_JL_OPTIONS[SUBFOLDERS]', $this->options('subfolders')); ?>
-									<p class="small text-muted"><?php echo WT_I18N::translate('If you set this option the results on the media list page will include subfolders.'); ?></p>
+									<p class="small text-muted"><?php echo I18N::translate('If you set this option the results on the media list page will include subfolders.'); ?></p>
 								</div>
 							</div>
 						</div>
@@ -429,13 +423,13 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 					<div class="panel-heading">
 						<h4 class="panel-title">
 							<a class="collapsed" href="#collapseTwo" data-target="#collapseTwo" data-toggle="collapse">
-								<?php echo WT_I18N::translate('Sort Topmenu items'); ?>
+								<?php echo I18N::translate('Sort Topmenu items'); ?>
 							</a>
 						</h4>
 					</div>
 					<div class="panel-collapse collapse" id="collapseTwo">
 						<div class="panel-heading">
-							<?php echo WT_I18N::translate('Click a row, then drag-and-drop to re-order the topmenu items. Then click the “save” button.'); ?>
+							<?php echo I18N::translate('Click a row, then drag-and-drop to re-order the topmenu items. Then click the “save” button.'); ?>
 						</div>
 						<div class="panel-body">
 							<?php
@@ -456,18 +450,18 @@ class justlight_theme_options_WT_Module extends WT_Module implements WT_Module_C
 			</div>
 			<button class="btn btn-primary" type="submit">
 				<i class="fa fa-check"></i>
-				<?php echo WT_I18N::translate('Save'); ?>
+				<?php echo I18N::translate('Save'); ?>
 			</button>
-			<button class="btn btn-primary" type="reset" onclick="if (confirm('<?php echo WT_I18N::translate('The settings will be reset to default. Are you sure you want to do this?'); ?>'))
+			<button class="btn btn-primary" type="reset" onclick="if (confirm('<?php echo I18N::translate('The settings will be reset to default. Are you sure you want to do this?'); ?>'))
 								window.location.href = 'module.php?mod=<?php echo $this->getName(); ?>&amp;mod_action=admin_reset';">
 				<i class="fa fa-recycle"></i>
-				<?php echo WT_I18N::translate('Reset'); ?>
+				<?php echo I18N::translate('Reset'); ?>
 			</button>
 		</form>
 		<?php
 	}
 
-	// Implement WT_Module_Config
+	// Implement ModuleConfigInterface
 	public function getConfigLink() {
 		return 'module.php?mod=' . $this->getName() . '&amp;mod_action=admin_config';
 	}
