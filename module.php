e900fa33894d631dfad4eb4527f77526b7e4194e
JustCarmen <carmen@justcarmen.nl>
2015-06-03 00:23:43 +0200

    PSR4: Namespaces - following webtrees base.

--- module.php
+++ module.php
@@ -17,6 +17,11 @@ namespace Fisharebest\Webtrees;
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Fisharebest\Webtrees\Module\AbstractModule;
+use Fisharebest\Webtrees\Module\ModuleConfigInterface;
+use Fisharebest\Webtrees\Controller\PageController;
+use Fisharebest\Webtrees\Query\QueryMedia;
+
 class JustLightThemeOptionsModule extends AbstractModule implements ModuleConfigInterface {
 
 	public function __construct() {
